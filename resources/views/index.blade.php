5fdb880ae206381406a8128bc19bd11984b78dd9
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-16 11:10:07 -0400

    new commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,6 @@
 </head>
 <body>
     <h1>PAYMENT FORM</h1>
-
     <form method="POST" action="{{ route('pay') }}" accept-charset="UTF-8" class="form-horizontal" role="form">
         <div class="row" style="margin-bottom:40px;">
             <div class="col-md-8 col-md-offset-2">
