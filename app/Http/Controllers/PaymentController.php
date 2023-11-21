299416538c01ccc67ee46c44f0fbb6a9204889cf
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-21 22:19:12 +0100

    new commit

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller; 
 use Illuminate\Support\Facades\Redirect; 
-use Unicodeveloper\Paystack\Facades\Paystack; 
+use Unicodeveloper\Paystack\Facades\Paystack;
 
 
 
